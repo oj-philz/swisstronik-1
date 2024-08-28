6d504b2e85bcbe53fcf61587a8e06a18375f28e7
Sunday Philip Ojogbane <sundayphilip069@gmail.com>
2024-08-28 21:29:02 +0000

    Initial commit

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,5 +1,7 @@
 const hre = require("hardhat");
+
 const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
 const sendShieldedTransaction = async (signer, destination, data, value) => {
   const rpcLink = hre.network.config.url;
   const [encryptedData] = await encryptDataField(rpcLink, data);
@@ -12,23 +14,19 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 };
 
 async function main() {
-  const contractAddress = "0x9f2439Bbd4d8B9f7896cC466054b9e93a9c6959e";
+  const contractAddress = "0xEacae57686BcD4FFeb207Aa95fcCdf5F70224B01"
   const [signer] = await hre.ethers.getSigners();
-
-  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contractFactory = await hre.ethers.getContractFactory("ZunXBT");
   const contract = contractFactory.attach(contractAddress);
-
-  const functionName = "mint100tokens";
-  const mint100TokensTx = await sendShieldedTransaction(
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
     signer,
     contractAddress,
-    contract.interface.encodeFunctionData(functionName),
+    contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
     0
   );
-
-  await mint100TokensTx.wait();
-
-  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+  await safeMintTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
 }
 
 main().catch((error) => {
